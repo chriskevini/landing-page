b7b27d6cc44ca15ee836e88442e60d66990e1064
chriskevini <chriskevini@gmail.com>
2022-10-11 01:52:30 -0700

    refactor padding as css class

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import ScrollTrigger from "gsap/dist/ScrollTrigger";
 import type { NextPage } from "next";
 import Head from "next/head";
 import { ReactElement, useEffect } from "react";
+import { RiMenuLine } from "react-icons/ri";
 gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
 
 function SectionHeading(props: { children: string; id: string }) {
@@ -23,14 +24,6 @@ function HeroMessage(props: { children: string }) {
     <h1 className="pt-[40vh] text-[min(12vw,100px)] leading-none">
       {props.children}
     </h1>
-  );
-}
-
-function Padding(props: { children: ReactElement | ReactElement[] }) {
-  return (
-    <div className="mx-auto h-screen w-10/12 max-w-3xl py-4">
-      {props.children}
-    </div>
   );
 }
 
@@ -82,8 +75,9 @@ const Home: NextPage = () => {
           >
             Chris Irineo
           </button>
-          <div className="hidden gap-12 text-sm font-extralight sm:flex md:gap-16 [&>*]:uppercase">
-            <button
+
+          <ul className="fixed inset-0 left-[50%] hidden items-center gap-12 text-sm font-extralight sm:flex md:gap-16 [&>*]:uppercase">
+            <li
               onClick={() =>
                 gsap.to(window, {
                   duration: 2,
@@ -92,8 +86,8 @@ const Home: NextPage = () => {
               }
             >
               My Work
-            </button>
-            <button
+            </li>
+            <li
               onClick={() =>
                 gsap.to(window, {
                   duration: 2,
@@ -102,8 +96,8 @@ const Home: NextPage = () => {
               }
             >
               My Skills
-            </button>
-            <button
+            </li>
+            <li
               onClick={() =>
                 gsap.to(window, {
                   duration: 2,
@@ -112,8 +106,12 @@ const Home: NextPage = () => {
               }
             >
               Contact Me
-            </button>
-          </div>
+            </li>
+          </ul>
+
+          <button className="text-xl sm:hidden">
+            <RiMenuLine />
+          </button>
         </div>
       </nav>
 
@@ -123,27 +121,28 @@ const Home: NextPage = () => {
           className="fixed h-screen w-screen"
         >
           <div className="absolute z-[-99999] h-full w-full bg-hero bg-cover bg-left-top "></div>
-          <Padding>
-            <div id="hero-elements">
-              <HeroMessage>UI/UX Design|</HeroMessage>
-              <button
-                onClick={() =>
-                  gsap.to(window, {
-                    duration: 2,
-                    scrollTo: { y: "#my-work", offsetY: 100 },
-                  })
-                }
-                className="pointer-events-auto mt-3 rounded-full bg-secondary py-1 px-5 uppercase text-black shadow-lg shadow-primary/30"
-              >
-                See My Work
-              </button>
-            </div>
-          </Padding>
+          <div
+            id="hero-elements"
+            className="padding h-screen"
+          >
+            <HeroMessage>UI/UX Design|</HeroMessage>
+            <button
+              onClick={() =>
+                gsap.to(window, {
+                  duration: 2,
+                  scrollTo: { y: "#my-work", offsetY: 100 },
+                })
+              }
+              className="pointer-events-auto mt-3 rounded-full bg-secondary py-1 px-5 uppercase text-black shadow-lg shadow-primary/30"
+            >
+              See My Work
+            </button>
+          </div>
         </div>
-        <div className="min-h-screen w-screen bg-primary pt-[150vh]">
-          <Padding>
+        <div className="w-screen bg-primary pt-[150vh]">
+          <div className="padding min-h-screen">
             <SectionHeading id="my-work">My Work</SectionHeading>
-          </Padding>
+          </div>
         </div>
       </main>
     </>
