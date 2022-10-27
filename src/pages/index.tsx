3b2191bbbe8518aa2aa48d1b4b11c8e6eba56fe3
chriskevini <chriskevini@gmail.com>
2022-10-27 15:57:18 -0700

    add phone overlay to project carousel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -125,38 +125,48 @@ function ProjectCarousel({ screens }: ProjectCarouselProps) {
   };
 
   return (
-    <Swiper
-      effect={"coverflow"}
-      grabCursor={true}
-      centeredSlides={true}
-      slidesPerView={2}
-      loop
-      navigation={true}
-      coverflowEffect={{
-        rotate: 0,
-        stretch: 0,
-        depth: 300,
-        modifier: 1,
-        slideShadows: true,
-      }}
-      modules={[EffectCoverflow, Navigation]}
-      className={
-        "md pointer-events-auto !mx-0 w-[150vw] [aspect-ratio:2/2!important] [--swiper-theme-color:white] [--swiper-navigation-size:1.75rem] [-webkit-mask-image:linear-gradient(to_right,transparent_0%,black_30%,black_70%,transparent_100%)] md:mt-[10%] md:mb-[30%] md:w-auto md:scale-150 md:[--swiper-navigation-size:1.25rem] md:portrait:w-full md:landscape:h-[50vh]"
-      }
-    >
-      {screens.map((screen, i) => (
-        <SwiperSlide
-          key={i}
-          className="[aspect-ratio:1/2!important]"
-        >
-          <Image
-            src={screen}
-            alt={screen}
-            layout="fill"
-          />
-        </SwiperSlide>
-      ))}
-    </Swiper>
+    <div className="relative md:mb-40 md:scale-125">
+      <Swiper
+        effect={"coverflow"}
+        grabCursor={true}
+        centeredSlides={true}
+        slidesPerView={2}
+        loop
+        navigation={true}
+        coverflowEffect={{
+          rotate: 0,
+          stretch: 0,
+          depth: 300,
+          modifier: 1,
+          slideShadows: true,
+        }}
+        modules={[EffectCoverflow, Navigation]}
+        className={
+          "pointer-events-auto !mx-0 !my-0 w-[150vw] [aspect-ratio:2/2!important] [--swiper-theme-color:white] [--swiper-navigation-size:1.75rem] [-webkit-mask-image:linear-gradient(to_right,transparent_0%,black_30%,black_70%,transparent_100%)] " +
+          "md:portrait:full md:mt-[10%] md:mb-[30%] md:w-auto md:[--swiper-navigation-size:1.25rem] md:landscape:h-[50vh]"
+        }
+      >
+        {screens.map((screen, i) => (
+          <SwiperSlide
+            key={i}
+            className="overflow-hidden rounded-3xl [aspect-ratio:1/2!important]"
+          >
+            <Image
+              src={screen}
+              alt={screen}
+              layout="fill"
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      <div className="absolute top-0 left-1/2 z-10 h-full w-1/2 -translate-x-1/2 scale-[105%]">
+        <Image
+          src="/iphone-frame.png"
+          alt=""
+          layout="fill"
+        />
+      </div>
+    </div>
   );
 }
 
@@ -321,5 +331,5 @@ const Home: NextPage = () => {
 
 export default Home;
 
-//TODO: paralax for section headers
+//TODO: fix carousel on large portrait viewport
 //TODO: add pagination to carousel
