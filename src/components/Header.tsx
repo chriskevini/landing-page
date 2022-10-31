cb414e34b0e237e9b1c915fd663f69506ce2f5f3
chriskevini <chriskevini@gmail.com>
2022-10-31 16:11:31 -0700

    move header to own file

---
+++ src/components/Header.tsx
@@ -0,0 +1,61 @@
+import gsap from "gsap";
+import { useState } from "react";
+import { RiMenuLine, RiCloseLine } from "react-icons/ri";
+import { NavItems } from "./NavItems";
+
+export function Header() {
+  const [navMenuIsOpen, setNavMenuIsOpen] = useState(false);
+
+  return (
+    <div>
+      <div className="fixed top-0 left-0 z-[4000] w-full overflow-hidden brightness-[80%]">
+        <div className="absolute z-[-4000] h-screen w-screen bg-hero bg-cover bg-left-top "></div>
+        <div className="padding flex justify-between py-4">
+          <button
+            onClick={() =>
+              gsap.to(window, {
+                duration: 2,
+                scrollTo: 0,
+              })
+            }
+            className="font-logo text-3xl leading-none tracking-[-0.095em]"
+          >
+            Chris Irineo
+          </button>
+          <nav className="hidden sm:block">
+            <ul className="h-full items-center gap-12 text-sm font-extralight sm:flex md:gap-16 [&>*]:cursor-pointer [&>*]:uppercase">
+              <NavItems />
+            </ul>
+          </nav>
+        </div>
+      </div>
+      <button
+        onClick={() => setNavMenuIsOpen((prev) => !prev)}
+        className="fixed right-[8.3vw] z-[5001] grid scale-75 place-items-center py-4 text-3xl sm:hidden"
+      >
+        <RiMenuLine
+          className={
+            "transition-all duration-300 " + (navMenuIsOpen ? "scale-0" : "")
+          }
+        />
+        <RiCloseLine
+          className={
+            "absolute transition-all duration-300 " +
+            (!navMenuIsOpen ? "scale-0" : "")
+          }
+        />
+      </button>
+      <nav
+        onClick={() => setNavMenuIsOpen(false)}
+        className={
+          "fixed top-0 right-0 z-[5000] block h-screen w-[60vw] bg-black/30 backdrop-blur-md transition-all duration-1000 sm:hidden" +
+          (navMenuIsOpen ? "" : " translate-x-full")
+        }
+      >
+        <ul className="flex list-disc flex-col gap-16 pt-32 pl-12 text-sm font-extralight uppercase [&>*]:cursor-pointer">
+          <NavItems />
+        </ul>
+      </nav>
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,6 @@ import ScrollTrigger from "gsap/dist/ScrollTrigger";
 import type { NextPage } from "next";
 import Head from "next/head";
 import { useEffect, useState } from "react";
-import { RiMenuLine, RiCloseLine } from "react-icons/ri";
 import { FaDraftingCompass, FaServer } from "react-icons/fa";
 import { MdOutlineDevices } from "react-icons/md";
 import { IoLogoGameControllerB } from "react-icons/io";
@@ -12,10 +11,10 @@ gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
 
 import { Section } from "../components/Section";
 import { HeroMessage } from "../components/HeroMessage";
-import { NavItems } from "../components/NavItems";
 import { ProjectInfo } from "../components/ProjectInfo";
 import { ProjectCarousel } from "../components/ProjectCarousel";
 import Skill from "../components/Skill";
+import { Header } from "../components/Header";
 
 const Home: NextPage = () => {
   const [navMenuIsOpen, setNavMenuIsOpen] = useState(false);
@@ -52,55 +51,7 @@ const Home: NextPage = () => {
           href="/logo.png"
         />
       </Head>
-
-      <div className="fixed top-0 left-0 z-[4000] w-full overflow-hidden brightness-[80%]">
-        <div className="absolute z-[-4000] h-screen w-screen bg-hero bg-cover bg-left-top "></div>
-        <div className="padding flex justify-between py-4">
-          <button
-            onClick={() =>
-              gsap.to(window, {
-                duration: 2,
-                scrollTo: 0,
-              })
-            }
-            className="font-logo text-3xl leading-none tracking-[-0.095em]"
-          >
-            Chris Irineo
-          </button>
-          <nav className="hidden sm:block">
-            <ul className="h-full items-center gap-12 text-sm font-extralight sm:flex md:gap-16 [&>*]:cursor-pointer [&>*]:uppercase">
-              <NavItems />
-            </ul>
-          </nav>
-        </div>
-      </div>
-      <button
-        onClick={() => setNavMenuIsOpen((prev) => !prev)}
-        className="fixed right-[8.3vw] z-[5001] grid scale-75 place-items-center py-4 text-3xl sm:hidden"
-      >
-        <RiMenuLine
-          className={
-            "transition-all duration-300 " + (navMenuIsOpen ? "scale-0" : "")
-          }
-        />
-        <RiCloseLine
-          className={
-            "absolute transition-all duration-300 " +
-            (!navMenuIsOpen ? "scale-0" : "")
-          }
-        />
-      </button>
-      <nav
-        onClick={() => setNavMenuIsOpen(false)}
-        className={
-          "fixed top-0 right-0 z-[5000] block h-screen w-[60vw] bg-black/30 backdrop-blur-md transition-all duration-1000 sm:hidden" +
-          (navMenuIsOpen ? "" : " translate-x-full")
-        }
-      >
-        <ul className="flex list-disc flex-col gap-16 pt-32 pl-12 text-sm font-extralight uppercase [&>*]:cursor-pointer">
-          <NavItems />
-        </ul>
-      </nav>
+      <Header />
 
       <main className="pointer-events-none bg-primary">
         <div
@@ -254,4 +205,3 @@ export default Home;
 //TODO: fix carousel on large portrait viewport
 //TODO: add pagination to carousel
 //TODO: fix blurry screens on carousel caused by scaling
-//TODO: move header to own file
