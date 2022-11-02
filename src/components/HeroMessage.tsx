761835b1daa3ac9c44016650e801b3b0c6109d93
chriskevini <chriskevini@gmail.com>
2022-11-01 20:46:09 -0700

    rename state var

--- src/components/HeroMessage.tsx
+++ src/components/HeroMessage.tsx
@@ -7,32 +7,25 @@ export function HeroMessage(props: {
   const line1 = "Hi! I'm Chris.";
   const line2 = "I do...";
   const line3 = ["UI/UX Design", "Front-End Dev", "Back-End Dev", "Game Dev"];
-  const [state, setState] = useState("line1");
+  const [currentLine, setCurrentLine] = useState(1);
   return (
     <div className="whitespace-pre-wrap pt-[20vh] text-[min(12vw,100px)] leading-[1.1]">
-      {state === "line1" ? (
-        <>
-          <TypeAnimation
-            sequence={[
-              "",
-              1000,
-              "Hi! :)",
-              1250,
-              line1,
-              () => setState("line2"),
-            ]}
-          />
-        </>
+      {currentLine === 1 ? (
+        <TypeAnimation
+          sequence={["", 1000, "Hi! :)", 1250, line1, () => setCurrentLine(2)]}
+        />
       ) : null}
-      {state === "line2" ? (
+
+      {currentLine === 2 ? (
         <>
           <div className="">{line1}</div>
           <TypeAnimation
-            sequence={["", 1000, line2, 500, () => setState("line3")]}
+            sequence={["", 1000, line2, 500, () => setCurrentLine(3)]}
           />
         </>
       ) : null}
-      {state === "line3" ? (
+
+      {currentLine === 3 ? (
         <>
           <div className="">{line1}</div>
           <div className="">{line2}</div>
