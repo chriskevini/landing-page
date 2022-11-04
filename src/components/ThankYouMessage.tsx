9e71e06915ce920f3841be4a7c5b610aec69f1ab
chriskevini <chriskevini@gmail.com>
2022-11-03 17:47:47 -0700

    add auto scroll to thank you message

--- src/components/ThankYouMessage.tsx
+++ src/components/ThankYouMessage.tsx
@@ -1,11 +1,23 @@
 import React, { useState } from "react";
 import { TypeAnimation } from "react-type-animation";
+import gsap from "gsap";
+
+const scrollToLine = (line: number) => {
+  gsap.to(window, {
+    duration: 0.5,
+    scrollTo: {
+      y: "#thank-you",
+      offsetY: window.innerHeight * 0.45 - line * 100,
+    },
+  });
+};
 
 function ThankYouMessage() {
   const [isDone, setIsDone] = useState(false);
   return (
     <div className="absolute top-0 whitespace-pre-wrap text-[min(6vw,50px)] leading-[2] transition-opacity duration-500 ">
       <div className="my-16 h-[84px]"></div>
+      <span id="thank-you"></span>
       {isDone ? (
         <p>
           {
@@ -16,16 +28,16 @@ function ThankYouMessage() {
         <TypeAnimation
           sequence={[
             "",
+            () => scrollToLine(0),
             1500,
             "Thank you!\n",
+            () => scrollToLine(1),
             1000,
             "Thank you!\nYour message has been received.\n",
+            () => scrollToLine(2),
             1000,
             "Thank you!\nYour message has been received.\nI will get back to you as soon as I can.",
-
-            () => {
-              setIsDone(true);
-            },
+            () => setIsDone(true),
           ]}
           className="animate-scale-y-in"
         />
